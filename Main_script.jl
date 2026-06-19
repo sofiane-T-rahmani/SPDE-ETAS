6858e6f87d276563352d466c33db44fc9ce0419d
Sofiane Rahmani <sofianerah12@gmail.com>
2026-06-19 15:58:23 +0200

    Update Main_script.jl

--- Main_script.jl
+++ Main_script.jl
@@ -1,3 +1,8 @@
+#Authors: Sofiane Taki-Eddine Rahmani, Gert Zöller, Sebastian Hainzl, Behnam Maleki Asayesh
+#Cite papar:  
+
+
+
 # ============================================================
 # ETAS-SPDE MCMC 
 # ============================================================
